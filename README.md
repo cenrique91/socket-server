0593a022f258428d5961f031e98d14146c579e6f
cenrique91 <cenrique91@hotmail.com>
2019-08-31 21:03:16 -0500

    Primer Commit Rest-Server

---
+++ README.md
@@ -0,0 +1,22 @@
+
+
+# Socket-Server
+
+
+Reconstruir modulos de Node
+````
+npm i
+````
+
+Generar el dist
+````
+tsc -w
+````
+
+Levantar el servidor, cualquiera de estos dos comandos
+````
+nodemon dist/
+node dist/
+````
+
+
---
+++ index.ts
@@ -0,0 +1,21 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const server = new Server();
+
+// BodyParser
+// aca abajo toma lo que sea que le poosteen y lo convierte en un objeto json
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// Rutas de Servicios
+server.app.use("/", router);
+
+server.start(() => {
+  console.log(`Servidor corriendo correctamente en el puerto ${server.port}`);
+});
